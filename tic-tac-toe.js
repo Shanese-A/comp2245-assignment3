b11b1059b6c48964cf7b194bcb25344b2a5c8dda
Shanese Archibald <shanese.archibald@gmail.com>
2024-10-28 15:00:10 -0400

    Exercise 5 - Implemented game reset functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -43,3 +43,12 @@ function checkWinner() {
         status.classList.add("you-won");
     }
 }
+document.getElementById("new-game-button").addEventListener("click", function() {
+    document.querySelectorAll("#board .square").forEach(square => {
+        square.textContent = "";
+        square.classList.remove("X", "O");
+    });
+    document.getElementById("status").textContent = "Move your mouse over a square and click to play an X or an O.";
+    document.getElementById("status").classList.remove("you-won");
+    turn = "X";
+});
