2f4b359d93fccab6a6d4805aa10311801be81e9c
Shanese Archibald <shanese.archibald@gmail.com>
2024-10-28 14:54:42 -0400

    Exercise 2- Added functionality to place X or O on click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,3 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     let squares = document.querySelectorAll("#board div");
     squares.forEach(square=> square.classList.add("square"));
 });
+
+let turn = "X";
+
+document.querySelectorAll("#board .square").forEach(square => {
+    square.addEventListener("click", function() {
+        if (!square.textContent) {
+            square.textContent = turn;
+            square.classList.add(turn);
+            turn = (turn === "X") ? "O" : "X";
+        }
+    });
+});
